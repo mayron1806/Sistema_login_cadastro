19f9dc3c58e259ba1e0d2ae0b9838f8a1a05faa1
mayron1806 <mayronfernandes01@gmail.com>
2021-09-14 09:16:07 -0300

    first commit

--- App/Models/LoginValidator.php
+++ App/Models/LoginValidator.php
@@ -11,8 +11,23 @@
         // chama a funcao para realizar a validacao do login
         // se der tudo certo retorna um objeto com os dados do usuario 
         public function validateLogin($user_data_input){
-            $user_data_return = $this->validator($user_data_input); 
-            return $user_data_return;
+            $query = 'SELECT * FROM tb_users';
+            $stmt = $this->database->query($query);
+
+            // aplica os dados a variavel $db_users_data
+            $db_users_data = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+            // procura o usuario 
+            foreach($db_users_data as $db_user_data){
+                // se encontrar o usuario vai verificar se a senha esta certa
+                if($db_user_data->name === $user_data_input->name){
+                    if ($db_user_data->password === $user_data_input->password){
+                        return $db_user_data;
+                    }else{
+                        return 'login_error';
+                    }
+                }
+            }
 
         }
         // realiza a validacao do login
